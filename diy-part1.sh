e6a011b4177e3fac32ed3c68d4ccc169d12e98e1
ABreadTree <62497798+ABreadTree@users.noreply.github.com>
2022-04-18 16:35:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+src-git Openclash https://github.com/vernesong/OpenClash
+src-git freifunk https://github.com/freifunk/openwrt-packages.git
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git packages https://github.com/coolsnowwolf/packages
+src-git luci https://github.com/coolsnowwolf/luci
+src-git routing https://git.openwrt.org/feed/routing.git
+src-git telephony https://git.openwrt.org/feed/telephony.git
